2a44bdace6500d88066831edb9a4c2395cfdeeb2
NickRashad <nrsternes@gmail.com>
2019-08-16 21:17:58 -0500

    Initialize App

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => {
+  return(
+    <div>
+      <h2>It's Go Time</h2>
+    </div>
+  )
+};
+
+ReactDOM.render(<App /> ,document.getElementById('app'));
